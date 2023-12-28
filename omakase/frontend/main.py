81366ef85fdf60752a505af5db28e11f901f562a
Xavier Fontaine <xavier.fontainec@gmail.com>
2023-12-28 12:26:07 +0100

    feat: base interface with all tabs

--- omakase/frontend/main.py
+++ omakase/frontend/main.py
@@ -4,7 +4,12 @@ Main page
 from nicegui import app, ui
 
 from omakase.frontend.login import Logger
-from omakase.frontend.tabs import StatsContent
+from omakase.frontend.tabs import (
+    AddDocContent,
+    AddSmallerContent,
+    EditNotesContent,
+    StatsContent,
+)
 from omakase.frontend.tabs.utils import TabConf
 from omakase.frontend.user import AUTH_STATUS_KEY
 
@@ -16,67 +21,26 @@ ADD_DOC_TAB_CONF = TabConf(
     name="add_doc",
     label="Add doc",
     icon="article",
-    content_generator=lambda: ui.label("bla"),
+    content_generator=AddDocContent,
 )
 ADD_SMALLER_TAB_CONF = TabConf(
     name="add_smaller",
     label="Add word",
     icon="record_voice_over",
-    content_generator=lambda: ui.label("bla"),
+    content_generator=AddSmallerContent,
 )
 EDIT_NOTES_TAB_CONF = TabConf(
     name="edit_notes",
     label="Edit notes",
     icon="mode_edit_outline",
-    content_generator=lambda: ui.label("bla"),
+    content_generator=EditNotesContent,
 )
 TAB_CONF_LIST: list[TabConf] = [
     STATS_TAB_CONF,
-    # ADD_DOC_TAB_CONF,
-    # ADD_SMALLER_TAB_CONF,
-    # EDIT_NOTES_TAB_CONF,
+    ADD_DOC_TAB_CONF,
+    ADD_SMALLER_TAB_CONF,
+    EDIT_NOTES_TAB_CONF,
 ]
-#
-#
-# class MainPageTemplate(ABC):
-#     def __init__(self):
-#         """Template for creating the main page"""
-#         pass
-#
-#     def display_page(self):
-#         with ui.header(elevated=True).classes("items-center justify-between"):
-#             self._header_content()
-#
-#     @abstractmethod
-#     def _header_content(self):
-#         pass
-#
-#     @abstractmethod
-#     def _body_content(self):
-#         pass
-#
-#
-# class MainPageLogged(MainPageTemplate):
-#     def _header_content(self):
-#         with ui.tabs() as self._tabs:
-#             for tab_conf in TAB_CONF_LIST:
-#                 ui.tab(
-#                     name=tab_conf.name,
-#                     label=tab_conf.label,
-#                     icon=tab_conf.icon,
-#                 ).bind_enabled_from(
-#                     target_object=app.storage.user,
-#                     target_name=AUTH_STATUS_KEY,
-#                 )
-#         # Login/logout
-#         logger = Logger()
-#         logger.make_button()
-#
-#     def _body_content(self):
-#         with ui.tab_panels(self._tabs, value=TAB_CONF_LIST[0].name).classes("w-full"):
-#             for tab_conf in TAB_CONF_LIST:
-#                 with ui.tab_panel(tab_conf.name):
-#                     tab_conf.content_generator().display_tab_content()
 
 
 def create_main_page():
---
+++ omakase/frontend/tabs/edit_notes.py
@@ -0,0 +1,14 @@
+"""
+Statistics tab
+"""
+from nicegui import ui
+
+from omakase.frontend.tabs.utils import TabContent
+
+
+class EditNotesContent(TabContent):
+    def __init__(self):
+        pass
+
+    def _display_if_logged(self):
+        ui.label("Hey, you are logged!")
